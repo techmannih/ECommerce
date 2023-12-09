235c00e3f635e126af568314a6c71f2c5a7c5cee
techmannih <manishchaudharyttt@gmail.com>
2023-12-09 10:11:20 +0530

    retrive user data

---
+++ server/controller/userController.js
@@ -0,0 +1,65 @@
+const { User } = require('../models/usermodel')
+const jwt = require('jsonwebtoken')
+
+const maxAge = 2 * 24 * 60 * 60;
+const mySecretKey = process.env.SECRET_KEY;
+
+
+function getToken(userID) {
+    return jwt.sign({ userID }, mySecretKey, {
+        expiresIn: maxAge
+    })
+}
+
+const handleErrors = (err) => {
+    const errors = {name:'', email: '', password: '' }
+
+    // error code
+    if (err.code == 11000) {
+        errors["email"] = "email already used"
+    }
+
+    // invalid email OR password - login
+    if(err.message == "invalid email"){
+        errors.email = "invalid email"
+    }
+    if(err.message == "invalid password"){
+        errors.password = "invalid password"
+    }
+
+    // validation of email & password - signup
+    if (err.message.includes("user validation failed")) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}
+
+module.exports.signup = async (req, res) => {
+    const { fullName, email, password } = req.body
+    console.log("signing up")
+    try {
+        const user = await User.create({ fullName, email, password })
+        res.status(201).send({ "success": "signed up successfully"})
+    }
+    catch (err) {
+        console.log("error signing up", err)
+        const errors = handleErrors(err)
+        res.status(400).send({ "errors": errors })
+    }
+}
+
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body
+    console.log("logging in")
+    try{
+        const user = await User.login(email, password)
+        res.status(202).send({"success": getToken(user._id)})
+    }
+    catch(err){
+        console.log("error logging in : ", err)
+        const errors = handleErrors(err)
+        res.status(401).send({"error" : errors})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors =require("cors")
 const connectDB=require("./db/conn")
+const userRoutes=require("./routes/userRoutes")
+require('dotenv').config();
 
 const port = process.env.PORT || 8880;
 
@@ -9,6 +12,10 @@ connectDB();
 app.get('/', function (req, res) {
   res.send('Hello');
 });
+
+app.use(express.json());
+app.use(cors());
+app.use(userRoutes);
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
