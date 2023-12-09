f636efa2214f1f7b561d7bc0679215d1b9584d72
techmannih <manishchaudharyttt@gmail.com>
2023-12-09 10:08:24 +0530

    hashed user's password

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 // schemas// its for user profile page
 const userSchema = new mongoose.Schema({
@@ -33,6 +35,28 @@ const userSchema = new mongoose.Schema({
 
 {timestamps:true},    
 );
+
+
+
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+// static methods
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email })
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password)
+        if (auth) {
+            return user
+        }
+        throw new Error("invalid password")
+    }
+    throw Error("invalid email")
+}
 
 // models
 const User = mongoose.model('user', userSchema)
