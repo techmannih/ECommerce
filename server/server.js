abf9bbb2357661532e407202983744dc71ef5fd2
techmannih <manishchaudharyttt@gmail.com>
2023-12-09 09:26:01 +0530

    connection setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB=require("./db/conn")
 
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8880;
+
+connectDB();
 
 app.get('/', function (req, res) {
   res.send('Hello');
