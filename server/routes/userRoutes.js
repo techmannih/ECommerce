3c775d27556c7f2d3a035eaab0f19d5939015957
techmannih <manishchaudharyttt@gmail.com>
2023-12-10 21:17:58 +0530

    add routes for delete profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
-const { signup, login} = require("../controller/userController")
+const { signup, login,deleteUserProfile} = require("../controller/userController")
 const router = Router()
 router.route('/login').post(login)
 router.route('/signup').post(signup)
+router.route("/profile/delete").delete(deleteUserProfile)
 module.exports = router
