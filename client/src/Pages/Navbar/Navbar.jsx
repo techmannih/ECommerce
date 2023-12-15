e6d5ae8e6dde0be3706eb247985bf6e9f48a6f22
techmannih <manishchaudharyttt@gmail.com>
2023-12-15 21:01:55 +0530

    style navbar

--- client/src/Pages/Navbar/Navbar.jsx
+++ client/src/Pages/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
+import logo from "../index.js";
+
 function Navbar() {
   return (
     <>
-      <div className="header flex justify-between bg-black text-white">
-        <div className="logo">Amazon</div>
-        <div className="became-seller">
-         <button className="">Become Seller</button> 
+      <div className="header flex justify-between bg-black text-white p-2 h-16 text-lg font-semibold items-center w-full">
+        <div className="logo">
+          <img src={logo} alt="amazon" />
         </div>
-        <div className="search">
-          <input type="text" className="" placeholder="search" />
-          <button>search</button>
+        <div className="become-seller">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">Become Seller</button>
         </div>
-        <div className="language">Eng & Hin</div>
-        <div className="account">Signup</div>
-        <div className="order">Order & Return</div>
-        <div className="cart">Cart</div>
+        <div className="search my-8">
+          <input type="text" className=" border p-2 rounded" placeholder="Search amazon.in" />
+          <button className="bg-blue-500 text-white px-4 py-2 ml-2 rounded">Search</button>
+        </div>
+        <div className="language text-white">Eng & Hin</div>
+        <div className="account text-white">Signup</div>
+        <div className="order text-white">Order & Return</div>
+        <div className="cart text-white">Cart</div>
       </div>
     </>
   );
---
+++ client/src/Pages/index.js
@@ -0,0 +1,5 @@
+import logo from "../Pages/images/amazonLogo.jpg"
+
+export default{
+logo
+}
