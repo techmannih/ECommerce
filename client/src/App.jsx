9c628c6129fa5e80e5692615e07b5170ce9e9802
techmannih <manishchaudharyttt@gmail.com>
2023-12-18 22:50:58 +0530

    add route for cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Cart from "./Pages/cart/cart";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </>
