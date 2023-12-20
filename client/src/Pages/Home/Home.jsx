46b2013ecac838eb233859ed4ebe290760c4d264
techmannih <manishchaudharyttt@gmail.com>
2023-12-20 11:30:51 +0530

    fetch and display data in json by searching

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,51 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('Phone');
   const [apiData, setApiData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${process.env.REACT_APP_QUERIES}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-          'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST,
-        },
-      };
+  const fetchData = async () => {
+    setIsLoading(true);
 
-      try {
-        const response = await fetch(url, options);
-
-        // Check if the response status is 429 (Too Many Requests)
-        if (response.status === 429) {
-          const retryAfter = parseInt(response.headers.get('Retry-After')) || 5; // Default to 5 seconds if no Retry-After header
-          console.log(`Rate limit exceeded. Retrying after ${retryAfter} seconds.`);
-          await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
-          return fetchData(); // Retry the request
-        }
-
-        const result = await response.json(); // Assuming the response is in JSON format
-        setApiData(result); // Update state with the API data
-        setIsLoading(false);
-      } catch (error) {
-        setError(error);
-        setIsLoading(false);
-      }
+    const pageSize = 50; // Adjust the page size as needed
+    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${encodeURIComponent(
+      searchQuery
+    )}&page=1&pageSize=${pageSize}&country=US&category_id=aps`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST,
+      },
     };
 
-    fetchData(); // Call the fetchData function when the component mounts
+    try {
+      const response = await fetch(url, options);
+
+      // Check if the response status is 429 (Too Many Requests)
+      if (response.status === 429) {
+        const retryAfter = parseInt(response.headers.get('Retry-After')) || 5;
+        console.log(`Rate limit exceeded. Retrying after ${retryAfter} seconds.`);
+        await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
+        return fetchData(); // Retry the request
+      }
+
+      const result = await response.json();
+      setApiData(result);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Fetch data when the component mounts
   }, []); // The empty dependency array ensures that this effect runs once when the component mounts
+
+  const handleSearch = () => {
+    fetchData(); // Fetch data with the updated searchQuery
+  };
 
   return (
     <div>
+      <div>
+        <label htmlFor="searchQuery">Search:</label>
+        <input
+          type="text"
+          id="searchQuery"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {apiData && (
         <div>
-          {/* Render your data here */}
           <h2>Data from API</h2>
           <pre>{JSON.stringify(apiData, null, 2)}</pre>
         </div>
