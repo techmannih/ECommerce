ff03c5f05a13c7d5c6e8669024442b461b3bf993
techmannih <manishchaudharyttt@gmail.com>
2023-12-20 10:53:00 +0530

    fetch and display data in json

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 export default function Home() {
   const [apiData, setApiData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,31 +16,40 @@ export default function Home() {
         },
       };
 
-try {
-  const response = await fetch(url, options);
+      try {
+        const response = await fetch(url, options);
 
-  // Check if the response status is 429 (Too Many Requests)
-  if (response.status === 429) {
-    const retryAfter = parseInt(response.headers.get('Retry-After')) || 5; // Default to 5 seconds if no Retry-After header
-    console.log(`Rate limit exceeded. Retrying after ${retryAfter} seconds.`);
-    await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
-    return fetchData(); // Retry the request
-  }
+        // Check if the response status is 429 (Too Many Requests)
+        if (response.status === 429) {
+          const retryAfter = parseInt(response.headers.get('Retry-After')) || 5; // Default to 5 seconds if no Retry-After header
+          console.log(`Rate limit exceeded. Retrying after ${retryAfter} seconds.`);
+          await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
+          return fetchData(); // Retry the request
+        }
 
-  const result = await response.json();
-  console.log(result);
-} catch (error) {
-  console.error(error);
-}
+        const result = await response.json(); // Assuming the response is in JSON format
+        setApiData(result); // Update state with the API data
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
     };
 
     fetchData(); // Call the fetchData function when the component mounts
   }, []); // The empty dependency array ensures that this effect runs once when the component mounts
 
   return (
-    <>
-      {/* You can use the 'apiData' state in your component */}
-      <div>{apiData}</div>
-    </>
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {apiData && (
+        <div>
+          {/* Render your data here */}
+          <h2>Data from API</h2>
+          <pre>{JSON.stringify(apiData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
   );
 }
