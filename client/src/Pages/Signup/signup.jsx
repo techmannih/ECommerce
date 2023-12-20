7c6ba806be9d76f7d4466b704a10828fd189d7b8
techmannih <manishchaudharyttt@gmail.com>
2023-12-20 21:21:08 +0530

    ädd Login design

---
+++ client/src/Pages/Signup/signup.jsx
@@ -0,0 +1,15 @@
+export default function Signup (){
+return(
+    
+    <div className=" p-8  h-screen flex items-center justify-center rounded-xl">
+    <div className="box border-black border-2 p-8 flex justify-center rounded-lg bg-neutral-200">
+      <div className="head flex-col flex text-center">
+          <h1 className="font-bold text-black text-3xl p-3 mb-8">SignUp</h1>
+          <button className="font-bold text-black bg-white text-lg p-2 px-4 m-2 rounded-3xl">Continue With Google</button>
+          <button className="font-bold text-black bg-orange-400 text-lg p-2 m-2 rounded-3xl">Login</button>
+      </div>
+    </div>
+  </div>
+)
+}
+//         // const dispatch: AppDispa
