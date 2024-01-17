1e0434569debf895370ef0e2d5f1d42e6cd72dad
techmannih <manishchaudharyttt@gmail.com>
2024-01-17 22:16:47 +0530

    add login and register

--- client/src/Container/MainRoutes.jsx
+++ client/src/Container/MainRoutes.jsx
@@ -4,7 +4,8 @@ import Home from "../Pages/home"
 import ContactUs from "../Pages/contact";
 import AboutUs from "../Pages/about";
 import Product from "../Pages/product";
-
+import Login from "../Auth/Login"
+import Register from "../Auth/Register";
 export default function MainRoutes() {
   return (
     <>
@@ -14,6 +15,8 @@ export default function MainRoutes() {
         <Route exact path="/contact" element={<ContactUs />} />
         <Route exact path="/about" element={<AboutUs />} />
         <Route exact path="/products" element={<Product />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     </>
   );}
