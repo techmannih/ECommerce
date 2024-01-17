8a134b58f43ead0f527899dbd1ed968fbd1ccc88
techmannih <manishchaudharyttt@gmail.com>
2024-01-17 20:55:42 +0530

    intial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Cart from "./Pages/cart/cart";
-import Address from "./Pages/address/address";
-import Login from "./Pages/Login/Login";
-import Signup from "./Pages/Signup/signup";
-import Order from "./Pages/order/order";
-import Profile from "./Pages/userprofile/profile";
-import Product from "./Pages/productDetails/product";
+
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/address" element={<Address />} />
-          <Route exact path="/cart" element={<Cart />} />
-          <Route exact path="/orders" element={<Order />} />
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/product" element={<Product />} />
-          <Route path="/product/:asin" element={<Product />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <>hello</>;
 }
 
 export default App;
--- client/src/Pages/index.js
+++
@@ -1,6 +0,0 @@
-import logo from "../Pages/images/amazonLogo.jpg"
-// import hero1 from "../Pages/images/"
-// import hero2 from "../Pages/images/"
-export default{
-logo
-}
