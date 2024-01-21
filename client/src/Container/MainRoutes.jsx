cbdfdd9237021c54f973e532d583fe00d5a0fc13
techmannih <manishchaudharyttt@gmail.com>
2024-01-21 22:33:36 +0530

    add routing of carts

--- client/src/Container/MainRoutes.jsx
+++ client/src/Container/MainRoutes.jsx
@@ -1,22 +1,33 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 import Navbar from "../Components/navbar";
-import Home from "../Pages/home"
+import Home from "../Pages/home";
 import ContactUs from "../Pages/contact";
 import AboutUs from "../Pages/about";
 import Product from "../Pages/product";
-import Login from "../Auth/Login"
+import Login from "../Auth/Login";
 import Register from "../Auth/Register";
+import Cart from "../Pages/cart";
+import Checkout from "../Pages/checkout";
+
 export default function MainRoutes() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/contact" element={<ContactUs />} />
-        <Route exact path="/about" element={<AboutUs />} />
-        <Route exact path="/products" element={<Product />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </>
-  );}
+    <Provider store={store}>
+      <>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<ContactUs />} />
+          <Route exact path="/about" element={<AboutUs />} />
+          <Route exact path="/products" element={<Product />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </>
+    </Provider>
+  );
+}
