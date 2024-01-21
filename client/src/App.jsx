dbd588f3eba2d5034ac66eb9faec346df6e1f667
techmannih <manishchaudharyttt@gmail.com>
2024-01-21 22:34:01 +0530

    add provider with store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainRoutes from "./Container/MainRoutes";
 
+import { Provider } from 'react-redux';
+import store from './redux/store'; 
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<MainRoutes />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,11 @@
 // index.js or App.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './redux/reducers/store';  // Make sure the path is correct
-
+import ReactDOM from 'react-dom'; // Make sure the path is correct
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <div className="">
     <App />
-  </Provider>,
+  </div>,
   document.getElementById('root')
 );
