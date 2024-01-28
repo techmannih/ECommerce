fa86155d04b68ebc713a25ce061d18e0a182b0c2
techmannih <manishchaudharyttt@gmail.com>
2024-01-28 21:12:06 +0530

    import as a default

---
+++ client/src/Pages/index.jsx
@@ -0,0 +1,6 @@
+export { default as Home } from './home';
+export { default as Product } from './product';
+export { default as AboutPage } from './about';
+export { default as ContactPage } from './contact';
+export { default as Cart } from './cart';
+export { default as Checkout } from './checkout';
