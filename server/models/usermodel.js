b9870c49f1b23c7290057d0d1aaf60fa47a2f509
techmannih <manishchaudharyttt@gmail.com>
2024-01-30 19:14:04 +0530

    add fullName is required

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -1,64 +1,62 @@
-const mongoose = require('mongoose')
-const { isEmail } = require('validator')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 // schemas// its for user profile page
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     fullName: {
-        type: String,
-        default: "John",
-      },
+      type: String,
+      required: true,
+    },
     email: {
-        type: String,
-        required: true,
-        unique: [true, 'email already used'],
-        validate: [isEmail, "email is not valid"]
+      type: String,
+      required: true,
+      unique: [true, "email already used"],
+      validate: [isEmail, "email is not valid"],
     },
     phoneNumber: {
-        type: String,
-        default: "",
-      },
+      type: String,
+      default: "",
+    },
     password: {
-        type: String,
-        required: true,
-        minLength: [6, "password must be 6 characters minimum"]
+      type: String,
+      required: true,
+      minLength: [6, "password must be 6 characters minimum"],
     },
     dateStamp: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
     },
     country: {
-        type: String,
-        default: "",
-      },
-},
+      type: String,
+      default: "",
+    },
+  },
 
-{timestamps:true},    
+  { timestamps: true }
 );
 
-
-
-
-userSchema.pre('save', async function (next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
-})
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 // static methods
 userSchema.statics.login = async function (email, password) {
-    const user = await this.findOne({ email })
-    if (user) {
-        const auth = await bcrypt.compare(password, user.password)
-        if (auth) {
-            return user
-        }
-        throw new Error("invalid password")
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
     }
-    throw Error("invalid email")
-}
+    throw new Error("invalid password");
+  }
+  throw Error("invalid email");
+};
 
 // models
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model("user", userSchema);
 
-module.exports = { User }
+module.exports = { User };
