9f29df32eb8eb28b0193831a5b156065624431de
techmannih <manishchaudharyttt@gmail.com>
2024-01-31 11:38:03 +0530

    update cart models

--- server/models/cartmodel.js
+++ server/models/cartmodel.js
@@ -1,43 +1,29 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 // schemas
-const cartSchema = new mongoose.Schema( {
-  çartId: {
-    required: true,
-    type: String,
-  },
-  
-    items: {
-      type: [
-        {
-          productId: {
-            type: String,
-          },
-          quantity: {
-            type: Number,
-            required: true,
-            min: [1, "Quantity can not be less then 1."],
-            default: 1,
-          },
-        },
-      ],
-      default: [],
+const cartSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: String,
+      required: true,
     },
-    totalItems: {
-        type: Number,
-        default: 0,
-      },
-    totalPrice: {
-        type: Number,
-        default: 0,
-      },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [1, "Quantity can not be less then 1."],
+      default: 1,
+    },
+    itemPrice: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
   },
 
-  { timestamps: true } 
+  { timestamps: true }
 );
 
 // models
-const Cart = mongoose.model('cart', cartSchema)
+const Cart = mongoose.model("cart", cartSchema);
 
-module.exports = { Cart }
+module.exports = { Cart };
