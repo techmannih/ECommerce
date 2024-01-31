ee9d8e6aebb649eabe2e1c451ba4a72cc27b5131
techmannih <manishchaudharyttt@gmail.com>
2024-01-31 11:38:52 +0530

    update cart routes

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -10,7 +10,7 @@ const router = Router()
 
 router.route("/cart/create").post(createCart)
 router.route("/cart").get(getAllCart)
-router.route("/cart/delete").get(clearAllCart)
-router.route("/cart/remove").put(removeItemInCart)
+router.route("/cart/delete").delete(clearAllCart)
+router.route("/cart/remove").delete(removeItemInCart)
 
 module.exports = router
