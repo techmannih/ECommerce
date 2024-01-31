d5dbbe06d2f6fab8d195d7c18dee24d83f175456
techmannih <manishchaudharyttt@gmail.com>
2024-01-31 16:51:17 +0530

    add routes for increase and decrease quantity

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -4,11 +4,15 @@ const {
     getAllCart,
     clearAllCart,
     removeItemInCart,
+    IncreaseItem,
+    DecreaseItem
 } = require('../controller/cartController');
 
 const router = Router();
 
 router.route("/cart/create").post(createOrUpdateCart);
+router.route("/cart/create/increase").put(IncreaseItem);
+router.route("/cart/create/decrease").put(DecreaseItem);
 router.route("/cart").get(getAllCart);
 router.route("/cart/delete").delete(clearAllCart);
 router.route("/cart/remove").delete(removeItemInCart);
