1db63070ced0456c719252b2d65962a5fa796d97
techmannih <manishchaudharyttt@gmail.com>
2024-02-15 18:51:08 +0530

    add payment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const userRoutes=require("./routes/userRoutes")
 const addressRoutes=require("./routes/addressRoutes")
 const cartRoutes=require("./routes/cartRoutes")
 const orderRoutes=require("./routes/orderRoutes")
+const paymentRoutes=require("./routes/paymentRoutes")
 require('dotenv').config();
 
 const port = process.env.PORT || 8880;
@@ -17,11 +18,16 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173"
+  })
+)
 app.use(userRoutes);
 app.use(addressRoutes);
 app.use(cartRoutes);
 app.use(orderRoutes)
+app.use(paymentRoutes)
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
