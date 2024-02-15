0e112fefd02aa6c8a77c3f4de539e553f618354a
techmannih <manishchaudharyttt@gmail.com>
2024-02-15 18:50:11 +0530

    add routes for payment

---
+++ server/routes/paymentRoutes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express')
+const {
+    makePayment,
+  } = require('../controller/paymentController');
+const router = Router()
+
+router.route('/payment').post(makePayment);
+module.exports = router
