8d663f79e24a190bca0ca35aa8c799ab9535d735
techmannih <manishchaudharyttt@gmail.com>
2024-08-19 09:53:46 +0530

    add cors origin

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const cors =require("cors")
-const connectDB=require("./db/conn")
-const userRoutes=require("./routes/userRoutes")
-const addressRoutes=require("./routes/addressRoutes")
-const cartRoutes=require("./routes/cartRoutes")
-const orderRoutes=require("./routes/orderRoutes")
-const paymentRoutes=require("./routes/paymentRoutes")
+const cors = require("cors");
+const connectDB = require("./db/conn");
+const userRoutes = require("./routes/userRoutes");
+const addressRoutes = require("./routes/addressRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 require('dotenv').config();
 
 const port = process.env.PORT || 8880;
@@ -18,16 +18,25 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://e-commerce-chi-olive.vercel.app"
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173"
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   })
-)
+);
+
 app.use(userRoutes);
 app.use(addressRoutes);
 app.use(cartRoutes);
-app.use(orderRoutes)
-app.use(paymentRoutes)
+app.use(orderRoutes);
+app.use(paymentRoutes);
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
