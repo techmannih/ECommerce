e714903312bbf1439b3c8b8407d03aed93322038
techmannih <manishchaudharyttt@gmail.com>
2024-08-22 14:37:02 +0530

    add new pass

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash(this.password, salt); //add salt to password
     next();
 });
 
