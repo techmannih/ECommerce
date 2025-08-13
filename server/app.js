98ae66a83d80fd6c14856341f9cdb310ff329863
techmannih <manishchaudharyttt@gmail.com>
2025-08-13 17:50:03 +0530

    add origin

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 const allowedOrigins = [
   'http://localhost:5173',
   'https://e-commerce-chi-olive.vercel.app',
+  'https://e-commerce-git-code-3dcfd9-manishchaudharytttgmailcoms-projects.vercel.app/',
 ];
 
 app.use(
